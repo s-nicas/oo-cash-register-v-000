e29ee8a479d63f8be37727df093809045a9335fc
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-02 18:32:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ end
 
 def apply_discount
   if @discount > 0
-  discount_amount =  @total * @discount
+  discount_amount =  @total / @discount
   @total = @total - discount_amount
   end
  "After the discount, the total comes to #{@total}."
